fa11c1c61283c1bdd73fdabed5eda99a664b314e
Sven Heyll <sven.heyll@lindenbaum.eu>
2019-09-19 17:02:34 +0200

    Fix haskell-nix hoogle tls certificate

--- nix-hoogle.sh
+++ nix-hoogle.sh
@@ -5,5 +5,5 @@ set -xe
 HERE="$(realpath "$(dirname "$0")")"
 
 pushd "${HERE}"
-nix-shell --pure --run "hoogle server --host='*' --https --cert=nix/cert.pem --key=nix/privkey.pem --port=8443 --local"
+nix-shell --pure --run "hoogle server --host='*' --https --cert=nix/hoogle-tls/cert.pem --key=nix/hoogle-tls/privkey.pem --port=8443 --local"
 popd
